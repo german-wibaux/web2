1c3188860d35b6a683b5410c5c71a4037b90f1b3
German <germanwibaux@gmail.com>
2017-09-03 15:11:20 -0300

    Fix ej8

--- ej8/ej8.php
+++ ej8/ej8.php
@@ -2,11 +2,18 @@
 
 <?php
 
-	if (isset($_POST["user"]) && isset($_POST["pass"])){
+	if (isset($_POST["user"]) && 
+		isset($_POST["pass"]) &&
+		$_POST["user"]!="" &&
+		$_POST["pass"]!="" ){
+		
 		$name=$_POST["user"];
 		$pass=$_POST["pass"];
 		echo "<h1>El nombre es: $name</h1>";
 		echo "La contraseña es: $pass";
+
+	} else {
+		echo "Introduzca nombre y contraseña";
 	}
 		
  ?>
--- ej8/js/ej8.js
+++ ej8/js/ej8.js
@@ -1,12 +1,12 @@
 
     
 $("#formulario").on("submit", function (event) {
-    console.log('entroooo');        
-        event.prevenDefault();
-    var serializedData = $(this).serialize();
+            
+        event.preventDefault();
+    let serializedData = $(this).serialize();
     console.log(serializedData);
         $.post("ej8.php",serializedData ,
             function(response){
-        alert("Response: " + response);
-    });
-    });
+        $("#data").html(response);
+});
+});
