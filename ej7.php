fdf6ccf47b31eb4b6fee9ee54cc9d5724dfa90b4
German <germanwibaux@gmail.com>
2017-09-03 11:56:32 -0300

    Change split for explode function

--- ej7.php
+++ ej7.php
@@ -4,8 +4,10 @@
 
 	$chain=$_GET['chain'];
 	
-	$chain_array=split('[,.-]',$chain);
+	$chain_array=explode(',',$chain);
 	$length=sizeof($chain_array);
+
+		
 
 	for ($i=0; $i < $length ; $i++) { 
 		echo"<p>".($chain_array[$i])."</p>";
